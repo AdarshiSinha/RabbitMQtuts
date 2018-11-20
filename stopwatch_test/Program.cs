3dd14174a1f4f55599d4128e7d61ed0ba11f664e
Adarshi Sinha <adarshisinha@gmail.com>
2018-11-20 14:04:04 +0530

    sample classes

---
+++ stopwatch_test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+namespace stopwatch_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch= new Stopwatch();
+            stopwatch.Start();
+            Thread.Sleep(5000);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedTicks);
+        }
+    }
+}
