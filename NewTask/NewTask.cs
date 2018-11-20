001c0b8178bb9a7b6266b2e15dd0d4364f5c7571
Adarshi Sinha <adarshisinha@gmail.com>
2018-11-20 10:37:24 +0530

    work queues added

---
+++ NewTask/NewTask.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace NewTask
+{
+    class NewTask
+    {
+        
+        public static void Main(string[] args)
+        {
+             var factory = new ConnectionFactory() { HostName = "localhost" };
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+        
+        var message = (args.Length > 0) ? string.Join(" ", args) : "Hello World!";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        var properties = channel.CreateBasicProperties();
+        properties.Persistent = true;
+
+        channel.BasicPublish(exchange: "",
+                     routingKey: "task_queue",
+                     basicProperties: properties,
+                     body: body);
+        }
+        }
+//          private static string GetMessage(string[] args)
+// {
+//     return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+// }
+   
+    }
+    
+   
+   
+}
+
+
